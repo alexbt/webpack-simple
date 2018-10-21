8bd0af55b743c86c796996b077f35d4cb03e2f9f
Your Name <you@example.com>
2018-10-21 18:21:01 +0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin'); // плагин для загрузки кода Vue
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [{
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      // это будет применяться к файлам `.js`
+      // А ТАКЖЕ к секциям `<script>` внутри файлов `.vue`
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      // это будет применяться к файлам `.css`
+      // А ТАКЖЕ к секциям `<style>` внутри файлов `.vue`
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+
+    ]
+  },
+  devServer: {
+    overlay: true
+  },
+  plugins: [
+    // убедитесь что подключили плагин!
+    new VueLoaderPlugin()
+  ]
+
+
+
+}
